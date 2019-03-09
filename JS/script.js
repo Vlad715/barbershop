bc26bafb7fdb318096b641b730682a5f5359a407
Vlad <vladislav24ua@gmail.com>
2019-03-09 16:34:26 +0200

    The first my commit

---
+++ JS/script.js
@@ -0,0 +1,56 @@
+'use stict';
+
+/*
+var number = 5;
+var string = "Hello!";
+var sym = Sumbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+*/
+
+console.log(4/0);
+console.log('string'/2);
+let something;
+console.log(something);
+
+let persone = {
+    name: "Jone",
+    age: 24,
+    isMarried: false
+};
+
+console.log(persone.age)
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[1]);
+
+// alert('Hello World');
+
+/* let answer = confirm('Are you here?');
+
+console.log(answer); */
+/*
+let answer = +prompt('Are you eighteen?', 'yes');
+
+console.log(typeof (answer) );
+
+console.log('arr' + '-object');
+console.log('4' + '-object');  */
+
+let incr = 20,
+    decr = 20;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(11%3);
+console.log('2' === 2)
+
+let isChecked = false,
+    isClose = false;
+
+console.log(isChecked || isClose);
